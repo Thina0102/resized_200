d1ece043e738a4dac7f96662177e939301e38e3d
Thina0102 <120770507+Thina0102@users.noreply.github.com>
2022-12-17 17:29:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ os.system("pip install sentence-transformers")
 
 
 setuptools.setup(
-    name="ZSIC",
+    name="zsic",
     version="1.0",
     author="PithiviDa @ Donkey Stereotype",
     author_email="",
     description="Zeroshot Image Classification",
     long_description="Zero Shot Image Classification equivalent for HuggingFace Zero Shot Text Classification - By Prithivi Da",
-    url="https://github.com/PrithivirajDamodaran/ZSIC.git",
+    url="https://github.com/Thina0102/zsic.git",
     packages=setuptools.find_packages(),
     install_requires=[],
     classifiers=[
